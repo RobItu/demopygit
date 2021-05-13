0263c8a2def5254df1a3738def2cc7ee5ddfd26b
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-12 18:19:24 -0600

    Testing

--- Odd or Even.py
+++ Odd or Even.py
@@ -1,8 +1,8 @@
-#This will be an Odd or Even game, where the user inputs a number and the program responds whether it's even or odd. 
+#This will be an Odd or Even game, where the user inputs a number and the program responds whether it's even or odd.
 Name = input('What is your name?\n')
 print('Hello ' + Name + ', please input any number')
-Number = input()
-if Number == 0:
+Number = int(input())
+if Number%2 == 0:
     print('Your number is an even number!')
 else:
     print('Your number is an odd number!')
